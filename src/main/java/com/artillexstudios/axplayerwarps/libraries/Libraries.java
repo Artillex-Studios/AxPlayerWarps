8a2a6d3a20a8edef14892ede43d040410967a37b
A248 <theanandbeh@gmail.com>
2025-04-02 10:51:34 -0500

    Relocate libraries and JDBC drivers where possible
    SQLite cannot be relocated due to use of native drivers.

--- src/main/java/com/artillexstudios/axplayerwarps/libraries/Libraries.java
+++ src/main/java/com/artillexstudios/axplayerwarps/libraries/Libraries.java
@@ -8,15 +8,15 @@ public enum Libraries {
 
     HIKARICP("com{}zaxxer:HikariCP:5.1.0", new Relocation("com{}zaxxer{}hikari", "com.artillexstudios.axplayerwarps.libs.hikari")),
 
-    MYSQL_CONNECTOR("com{}mysql:mysql-connector-j:8.0.33"),
+    MYSQL_CONNECTOR("com{}mysql:mysql-connector-j:8.0.33", new Relocation("com{}mysql", "com.artillexstudios.axplayerwarps.libs.mysql")),
 
     SQLITE("org{}xerial:sqlite-jdbc:3.42.0.0"),
 
-    H2_JDBC("com{}h2database:h2:2.1.214"),
+    H2_JDBC("com{}h2database:h2:2.1.214", new Relocation("org{}h2", "com.artillexstudios.axplayerwarps.libs.h2database")),
 
-    POSTGRESQL("org{}postgresql:postgresql:42.5.4"),
+    POSTGRESQL("org{}postgresql:postgresql:42.5.4", new Relocation("org{}postgresql", "com.artillexstudios.axplayerwarps.libs.postgresql")),
 
-    COMMONS_COLLECTIONS("org{}apache{}commons:commons-collections4:4.5.0-M2");
+    COMMONS_COLLECTIONS("org{}apache{}commons:commons-collections4:4.5.0-M2", new Relocation("org{}apache{}commons{}collections4", "com.artillexstudios.axplayerwarps.libs.apachecollections4"));
 
     private final Library library;
 
